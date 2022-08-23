7ed1fbba8bc408ea5e767315b440fe016c574936
tctienn <94751859+tctienn@users.noreply.github.com>
2022-08-23 19:43:35 +0700

    Add files via upload

---
+++ web/bai ket thuc mon web/html/loc1.js
@@ -0,0 +1,12 @@
+var a=document.getElementById('dt');
+var a2=document.getElementById('mt');
+var a3=document.getElementById('k');
+
+function click1()
+{
+    a=document.getElementById('dt').style.backgroundColor='black';
+    alert("hmm");
+     
+    //document.getElementById('dt').style.backgroundColor='black';
+    
+}
---
+++ web/bai ket thuc mon web/js/loc.js
@@ -0,0 +1,199 @@
+var a;
+var a2=document.getElementById('mt');
+var a3=document.getElementById('k');
+var aa=0;
+function click1(aa)
+{
+
+    if(aa==1)
+    {
+        if(document.getElementById('dt').style.backgroundColor=='black')
+        {
+            
+             a=document.getElementById('dt').style.backgroundColor=('#1a1a66');
+        }
+        else
+        {
+            
+            a=document.getElementById('dt').style.backgroundColor=('black');
+        }
+           
+        
+
+    }
+     if(aa==2)
+     {
+        if(document.getElementById('mt').style.backgroundColor=='black')
+        {
+            
+             a=document.getElementById('mt').style.backgroundColor=('#1a1a66');
+        }
+        else
+        {
+            
+            a=document.getElementById('mt').style.backgroundColor=('black');
+        }
+     }
+    
+     if(aa==3)
+     {
+        if(document.getElementById('k').style.backgroundColor=='black')
+        {
+            
+             a=document.getElementById('k').style.backgroundColor=('#1a1a66');
+        }
+        else
+        {
+            
+            a=document.getElementById('k').style.backgroundColor=('black');
+        }
+     }
+
+     if(aa==4)
+     {
+        if(document.getElementById('dtt').style.backgroundColor=='black')
+        {
+            
+             a=document.getElementById('dtt').style.backgroundColor=('#1a1a66');
+        }
+        else
+        {
+            
+            a=document.getElementById('dtt').style.backgroundColor=('black');
+        }
+     }
+     if(aa==5)
+     {
+        if(document.getElementById('mtt').style.backgroundColor=='black')
+        {
+            
+             a=document.getElementById('mtt').style.backgroundColor=('#1a1a66');
+        }
+        else
+        {
+            
+            a=document.getElementById('mtt').style.backgroundColor=('black');
+        }
+     }
+     if(aa==6)
+     {
+        if(document.getElementById('tn').style.backgroundColor=='black')
+        {
+            
+             a=document.getElementById('tn').style.backgroundColor=('#1a1a66');
+        }
+        else
+        {
+            
+            a=document.getElementById('tn').style.backgroundColor=('black');
+        }
+     }
+     if(aa==7)
+     {
+        if(document.getElementById('chuot').style.backgroundColor=='black')
+        {
+            
+             a=document.getElementById('chuot').style.backgroundColor=('#1a1a66');
+        }
+        else
+        {
+            
+            a=document.getElementById('chuot').style.backgroundColor=('black');
+        }
+     }
+     if(aa==8)
+     {
+        if(document.getElementById('pin').style.backgroundColor=='black')
+        {
+            
+             a=document.getElementById('pin').style.backgroundColor=('#1a1a66');
+        }
+        else
+        {
+            
+            a=document.getElementById('pin').style.backgroundColor=('black');
+        }
+     }
+
+
+     if(aa==9)
+     {
+        if(document.getElementById('op').style.backgroundColor=='black')
+        {
+            
+             a=document.getElementById('op').style.backgroundColor=('#1a1a66');
+        }
+        else
+        {
+            
+            a=document.getElementById('op').style.backgroundColor=('black');
+        }
+     }
+
+     if(aa==10)
+     {
+        if(document.getElementById('$').style.backgroundColor=='black')
+        {
+            
+             a=document.getElementById('$').style.backgroundColor=('#1a1a66');
+        }
+        else
+        {
+            
+            a=document.getElementById('$').style.backgroundColor=('black');
+        }
+     }
+     if(aa==11)
+     {
+        if(document.getElementById('$$').style.backgroundColor=='black')
+        {
+            
+             a=document.getElementById('$$').style.backgroundColor=('#1a1a66');
+        }
+        else
+        {
+            
+            a=document.getElementById('$$').style.backgroundColor=('black');
+        }
+     }
+     if(aa==12)
+     {
+        if(document.getElementById('$$$').style.backgroundColor=='black')
+        {
+            
+             a=document.getElementById('$$$').style.backgroundColor=('#1a1a66');
+        }
+        else
+        {
+            
+            a=document.getElementById('$$$').style.backgroundColor=('black');
+        }
+     }
+     if(aa==13)
+     {
+        if(document.getElementById('$$$$').style.backgroundColor=='black')
+        {
+            
+             a=document.getElementById('$$$$').style.backgroundColor=('#1a1a66');
+        }
+        else
+        {
+            
+            a=document.getElementById('$$$$').style.backgroundColor=('black');
+        }
+     }
+     if(aa==14)
+     {
+        if(document.getElementById('$$$$$').style.backgroundColor=='black')
+        {
+            
+             a=document.getElementById('$$$$$').style.backgroundColor=('#1a1a66');
+        }
+        else
+        {
+            
+            a=document.getElementById('$$$$$').style.backgroundColor=('black');
+        }
+     }
+
+}
