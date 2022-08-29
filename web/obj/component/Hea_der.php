619591c634513348327edf69222e7f1bdaf905ae
tctienn <mainichichimvm@gmail.com>
2022-08-29 13:32:58 +0700

    tách header tạo component

---
+++ web/obj/component/Hea_der.php
@@ -0,0 +1,42 @@
+<?php
+    // mã cần nhúng
+    // css : <script src="https://code.iconify.design/iconify-icon/1.0.0-beta.3/iconify-icon.min.js"></script>
+    // bootrap:<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"><link rel="stylesheet" href="css/trangchu.css">
+
+?>
+    <div class="container-fluid" style="padding: 0; ">
+        <!--header-->
+        <div class="container-fluid" id="header">
+            <div class="container row" id="onhead">
+                <div class="col-md-9">
+                    <div class="logo row">
+                        <i> nhà thuốc</i>
+                        <b>Long Châu</b>
+                    </div>
+                </div>
+                <div class="col-md-3" id="right_head">
+                    <iconify-icon icon="akar-icons:file" style=" color: white; margin-top: -15px;" width="27"
+                        height="31 ; ">
+                    </iconify-icon> &nbsp;
+                    <p>
+                        Tra cứu <br>Lịch sử đơn hàng
+                    </p> &nbsp; &nbsp;
+                    <iconify-icon icon="el:shopping-cart" style="color: white; margin-top: -15px;" width="27"
+                        height="31"></iconify-icon> &nbsp;
+                    <p style="margin-top: 6%;">
+                        Giỏ hàng
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <!--end header-->
+
+       
+        <!--end food-->
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+            crossorigin="anonymous"></script>
+
+    </div>
---
+++ web/obj/component/test.php
@@ -0,0 +1,8 @@
+
+<?php
+    // require_once("component/hea_der.php");
+    // require_once("../html/index.php")
+    require("../html/index.php")
+?>
+
+<!--  -->
