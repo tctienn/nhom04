97e583df9aaa2aa888a046f5c6025a06b5e6f152
ThaoDan <thaoto2801@gmail.com>
2022-08-30 00:17:11 +0700

    cham soc suc khoe

---
+++ web/obj/html/blog.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/trangchu.css">
+    <script src="https://code.iconify.design/iconify-icon/1.0.0-beta.3/iconify-icon.min.js"></script>
+    <title>Blog</title>
+</head>
+
+<body>
+    <!--header-->
+    <?php
+        require_once("../component/hea_der.php")
+    ?>
+    <br>
+
+    <div class="container row" style="margin: auto;">
+        <div class="col-md-7" style="border-right:solid 1px rgb(169, 169, 169) ;">
+            <h2>Tác dụng tuyệt vời của niacinamide trong chu trình chăm sóc da</h2>
+            <i>Chủ Nhật ngày 28/08/2022</i>
+            <br>
+            <hr>
+            <p>
+                Niacinamide là một loại vitamin có lợi trong thực phẩm và được đưa vào cơ thể bằng nhiều cách như ăn
+                uống hoặc thoa trực tiếp lên da. Niacinamide thuộc nhóm vitamin B và phức hợp vitamin B3, trong các loại
+                sản phẩm chăm sóc da, chất này được sử dụng để loại bỏ các vấn đề da liên quan đến mụn và lỗ chân lông.
+
+                Niacinamide thường được đánh giá là thành phần hiếm hoi có nhiều lợi ích trong chăm sóc da. Đặc biệt là
+                tác dụng phụ xảy ra không đáng kể và hoàn toàn có thể khắc phục. Vì vậy, điều đó có thể thuyết phục ngay
+                cả với những khách hàng khó tính hoặc những người sở hữu làn da nhạy cảm.
+            </p>
+            <div>
+                <img src="../image/tac-dung-tuyet-voi-cua-niacinamide-trong-chu-trinh-cham-soc-da-1.jpeg"
+                    style="width: 90%; aspect-ratio: 3/2;" alt="">
+                <center><i>Niacinamide thuộc nhóm vitamin B và phức hợp vitamin B3</i></center>
+            </div>
+            <p>
+            <h4><b>Tác dụng của Niacinamide trong làm đẹp</b></h4>
+            Hiện nay chưa có nhiều nghiên cứu cụ thể về tác động của vitamin B đến kích thước lỗ chân lông. Tuy nhiên
+            niacinamide thì lại có tác dụng rõ ràng trong việc thu nhỏ lỗ chân lông, giúp lỗ chân lông trở nên thông
+            thoáng, đẩy lùi tình trạng bít tắc. Sử dụng theo thời gian cùng với hoạt động tế bào, lỗ chân lông được làm
+            sạch và duy trì bởi niacinamide sẽ khôi phục về kích thước ban đầu. Đối với niacinamide nồng độ cao cũng sẽ
+            giúp cải thiện được tình trạng da sần sùi – vấn đề xảy ra khi lỗ chân lông phải chịu tác động từ các yếu tố
+            như ánh nắng mặt trời hoặc khói bụi.
+            <div>
+                <br>
+                <img src="../image/tac-dung-tuyet-voi-cua-niacinamide-trong-chu-trinh-cham-soc-da-2.jpg"
+                    style="width: 90%; aspect-ratio: 3/2;" alt="">
+                <center><i> ảnh minh họa </i></center>
+                <br>
+            </div>
+            Hàng rào bảo vệ da giúp da hạn chế và tránh khỏi những xâm nhập có hại từ môi trường bên ngoài khiến làn da
+            mất cân bằng ẩm. Sự có mặt và tham gia của niacinamide trong chu trình chăm sóc da sẽ giúp tái tạo và củng
+            cố hàng rào bảo vệ da trở nên kiên cố, sản sinh các ceramide để lấy lại sự trẻ trung, mềm mịn vốn có của làn
+            da.
+            </p>
+            <div style="float: right ;">Nguồn: Tổng hợp</div>
+            <br>
+            <div style="clear: right ;">
+                <small>
+                    <b>Lưu ý:</b>
+                    Thông tin trong bài viết chỉ mang tính chất tham khảo, vui lòng
+                    liên hệ với Bác sĩ, Dược
+                    sĩ hoặc
+                    chuyên viên y tế để được tư vấn cụ thể.
+                </small>
+            </div>
+            <hr>
+            <br>
+            <div class="row" style="display: flex;
+            justify-content: space-evenly;">
+                <h4>Bài viết liên quan</h4>
+                <hr>
+                <div class="col-md-3">
+                    <img src="../image/da-bi-chay-nang-dung-lam-nhung-dieu-nay-UalHI-1515062804_small.jpg" style="    width: 100%;
+                    aspect-ratio: 3/2;" alt="">
+                    <small>
+                        Da bị cháy nắng: đừng làm những điều này để tình hình tệ hơn
+                        <small>Thứ Sáu ngày 29/09/2017</small>
+                    </small>
+                </div>
+                <div class="col-md-3">
+                    <img src="../image/da-bi-chay-nang-dung-lam-nhung-dieu-nay-UalHI-1515062804_small.jpg" style="    width: 100%;
+                    aspect-ratio: 3/2;" alt="">
+                    <small>
+                        Da bị cháy nắng: đừng làm những điều này để tình hình tệ hơn
+                        <small>Thứ Sáu ngày 29/09/2017</small>
+                    </small>
+                </div>
+                <div class="col-md-3">
+                    <img src="../image/da-bi-chay-nang-dung-lam-nhung-dieu-nay-UalHI-1515062804_small.jpg" style="    width: 100%;
+                    aspect-ratio: 3/2;" alt="">
+                    <small>
+                        Da bị cháy nắng: đừng làm những điều này để tình hình tệ hơn
+                        <small>Thứ Sáu ngày 29/09/2017</small>
+                    </small>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-5">
+            <h4>Bài nổi bật</h4>
+            <hr style="width: 80%; margin: auto;">
+            <br>
+
+            <div class="container-fluid " style="display: flex; margin-bottom: 20px;">
+                <img src="../image/tai-sao-moi-cua-ban-bi-nut-ne-cach-duong-moi-ban-nen-biet-styty-1648789248_small.jpg"
+                    style=" padding: 0; width: 24%; aspect-ratio: 6/5;" alt="">
+                <div style="display: flex; flex-direction: column; justify-content: space-between;">
+                    <h5>Tại sao môi của bạn bị nứt nẻ? Cách dưỡng môi bạn nên biết</h5>
+                    <small><i>Thứ Hai ngày 21/03/2022</i></small>
+                </div>
+            </div>
+            <div class="container-fluid " style="display: flex; margin-bottom: 20px;">
+                <img src="../image/tai-sao-moi-cua-ban-bi-nut-ne-cach-duong-moi-ban-nen-biet-styty-1648789248_small.jpg"
+                    style=" padding: 0; width: 24%; aspect-ratio: 6/5;" alt="">
+                <div style="display: flex; flex-direction: column; justify-content: space-between;">
+                    <h5>Tại sao môi của bạn bị nứt nẻ? Cách dưỡng môi bạn nên biết</h5>
+                    <small><i>Thứ Hai ngày 21/03/2022</i></small>
+                </div>
+            </div>
+
+            <div class="container-fluid " style="display: flex; margin-bottom: 20px;">
+                <img src="../image/tai-sao-moi-cua-ban-bi-nut-ne-cach-duong-moi-ban-nen-biet-styty-1648789248_small.jpg"
+                    style=" padding: 0; width: 24%; aspect-ratio: 6/5;" alt="">
+                <div style="display: flex; flex-direction: column; justify-content: space-between;">
+                    <h5>Tại sao môi của bạn bị nứt nẻ? Cách dưỡng môi bạn nên biết</h5>
+                    <small><i>Thứ Hai ngày 21/03/2022</i></small>
+                </div>
+            </div>
+
+            <div class="container-fluid " style="display: flex; margin-bottom: 20px;">
+                <img src="../image/tai-sao-moi-cua-ban-bi-nut-ne-cach-duong-moi-ban-nen-biet-styty-1648789248_small.jpg"
+                    style=" padding: 0; width: 24%; aspect-ratio: 6/5;" alt="">
+                <div style="display: flex; flex-direction: column; justify-content: space-between;">
+                    <h5>Tại sao môi của bạn bị nứt nẻ? Cách dưỡng môi bạn nên biết</h5>
+                    <small><i>Thứ Hai ngày 21/03/2022</i></small>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <br>
+    <hr>
+    <br>
+    <!--footer-->
+    <?php
+        require_once("../component/footer.php")
+    ?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
--- web/obj/login.php
+++ web/obj/login.php
@@ -73,15 +73,18 @@ if ($action == "login") {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <script src="//maxcdn.bootstrapcdn.com/b    ootstrap/4.1.1/js/bootstrap.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <style type="text/css">
         body {
-            margin: 0;
+            /* margin: 0; */
+            margin-top: 100px;
             padding: 0;
-            background-color: #17a2b8;
+            /* background-color: #0277bd; */
             height: 100vh;
             background-image: url('https://acegif.com/wp-content/gifs/starfall-gif-46.gif');
+            
+        
             background-size: 100% 100%;
         }
 
@@ -125,17 +128,17 @@ if ($action == "login") {
                 <div id="login-column" class="col-md-6">
                     <div id="login-box" style='background-color: #ffffffcc; border-radius: 10px; margin-top:40px' class="col-md-12">
                         <form id="login-form" class="form" action="" method="post">
-                            <h3 class="text-center text-info">Đăng nhập</h3>
+                            <h3 class="text-center text-info">ĐĂNG NHẬP</h3>
                             <div class="form-group">
-                                <label for="username" class="text-info">tên đăng nhập/gmail:</label><br>
+                                <label for="username" class="text-info">Tên đăng nhập/gmail:</label><br>
                                 <input type="text" name="username" id="username" class="form-control" value="<?= $username ?>">
                             </div>
                             <div class="form-group">
-                                <label for="password" class="text-info">mật khẩu:</label><br>
+                                <label for="password" class="text-info">Mật khẩu:</label><br>
                                 <input type="password" name="password" id="password" class="form-control" value="<?= $password ?>">
                             </div>
                             <div class="form-group">
-                                <label for="remember-me" class="text-info"><span>nhớ mật khẩu</span> <span><input id="remember-me" name="remember-me" type="checkbox"></span></label><br>
+                                <label for="remember-me" class="text-info"><span>Nhớ mật khẩu</span> <span><input id="remember-me" name="remember-me" type="checkbox"></span></label><br>
                                 <input type="hidden" id="action" name="action" value="login" />
                                 <input type="submit" name="submit" class="btn btn-info btn-md" value="Đăng nhập">
                             </div>
