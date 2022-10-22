f1dbb290d2b9ea153979c47bf40f1353a6e298bd
tctienn <mainichichimvm@gmail.com>
2022-10-23 00:56:18 +0700

    deleted:    web/obj/New folder/class.Cart.php 	deleted:    web/obj/New folder/example.php 	deleted:    web/obj/New folder/vnpay_php/vnpay_php/assets/bootstrap.min.css 	deleted:    web/obj/New folder/vnpay_php/vnpay_php/assets/jquery-1.11.3.min.js 	deleted:    web/obj/New folder/vnpay_php/vnpay_php/assets/jumbotron-narrow.css 	deleted:    web/obj/New folder/vnpay_php/vnpay_php/config.php 	deleted:    web/obj/New folder/vnpay_php/vnpay_php/index.php 	deleted:    web/obj/New folder/vnpay_php/vnpay_php/nbproject/private/private.properties 	deleted:    web/obj/New folder/vnpay_php/vnpay_php/nbproject/private/private.xml 	deleted:    web/obj/New folder/vnpay_php/vnpay_php/nbproject/project.properties 	deleted:    web/obj/New folder/vnpay_php/vnpay_php/nbproject/project.xml 	deleted:    web/obj/New folder/vnpay_php/vnpay_php/vnpay_create_payment.php 	deleted:    web/obj/New folder/vnpay_php/vnpay_php/vnpay_ipn.php 	deleted:    web/obj/New folder/vnpay_php/vnpay_php/vnpay_query.php 	deleted:    web/obj/New folder/vnpay_php/vnpay_php/vnpay_refund.php 	deleted:    web/obj/New folder/vnpay_php/vnpay_php/vnpay_return.php 	deleted:    web/obj/component/test.php 	deleted:    web/obj/cssk.php 	deleted:    web/obj/functions/create_user.php 	deleted:    web/obj/functions/functions.php 	deleted:    web/obj/test.css 	deleted:    web/obj/test.php 	deleted:    web/obj/ui.txt

--- web/obj/component/test.php
+++
@@ -1,8 +0,0 @@
-
-<?php
-    // require_once("component/hea_der.php");
-    // require_once("../html/index.php")
-    require("../html/index.php")
-?>
-
-<!--  -->
--- web/obj/functions/create_user.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require_once("../classes/dbConnection.php");
-
-    $dbConnection = new dbConnection();
-    $conn = $dbConnection->getConnection();
-    $sql = "INSERT INTO 'user' ('id', 'usename', 'password', 'is_admin', 'create_time') VALUES (NULL, '$username', MD5('$password'), '0', '11')";
-    
-
-?>
